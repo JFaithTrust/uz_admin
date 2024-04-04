87534c3ae0a0a93d0c59397a41ad5a7aa78c3e66
Narzullayev Javohir <narzullayevjavohir91@gmail.com>
2024-04-04 17:50:33 +0500

    new page

--- app/dashboard/jobs/[viewId]/page.tsx
+++ app/dashboard/jobs/[viewId]/page.tsx
@@ -2,7 +2,9 @@
 
 import { Badge } from "@/components/ui/badge";
 import { useJobStore } from "@/lib/store/jobs-store";
+import { formatNumber } from "@/lib/utils";
 import React, { useEffect } from "react";
+import JobCard from "./job-card";
 
 const ViewId = ({ params }: { params: { viewId: string } }) => {
   const { job, fetchJobsId } = useJobStore();
@@ -11,11 +13,24 @@ const ViewId = ({ params }: { params: { viewId: string } }) => {
     fetchJobsId(params.viewId);
   }, [fetchJobsId, params.viewId]);
 
-  console.log(job);
+  function getTime() {
+    const date = new Date(job.createDate);
+    let day: string | number = date.getDay();
+    day = day < 10 ? "0" + day : day;
+    let month: string | number = date.getMonth();
+    month = month < 10 ? "0" + month : month;
+    let year: string | number = date.getFullYear();
+
+    if (year < 10) {
+      year = "0" + year;
+    }
+
+    return day + "." + month + "." + year + "-yil";
+  }
 
   return (
     <div>
-      <h1 className="text-2xl font-bold">{job.title}</h1>
+      <h1 className="text-xl font-bold">{job.title}</h1>
       <div className="flex flex-row items-center gap-x-10 mt-8">
         <div className="flex flex-col items-center">
           <h3>Ish turi</h3>
@@ -24,6 +39,22 @@ const ViewId = ({ params }: { params: { viewId: string } }) => {
         <div className="flex flex-col items-center">
           <h3>Jinsi</h3>
           <Badge variant="secondary">{job.gender}</Badge>
+        </div>
+      </div>
+      <div className="mt-12">
+        <div className="grid grid-cols-3 gap-y-16 gap-x-5">
+          <JobCard word="Maosh" jobTitle={job.salary} />
+          <JobCard word="Ish vaqti" jobTitle={job.workingTime} />
+          <JobCard word="Sharoitlar" jobTitle={job.benefit} />
+          <JobCard
+            word="Yosh uchun talab"
+            jobTitle={job.minAge + " dan " + job.maxAge + " gacha"}
+          />
+          <JobCard word="Tajriba" jobTitle={job.requirement} />
+          <JobCard word="E'lon berilgan sana:" jobTitle={job.title} />
+          <JobCard word="Telefon raqami:" jobTitle={job.phoneNumber} />
+          <JobCard word="E'lon berilgan sana:" jobTitle={getTime()} />
+          <JobCard word="Ish kuni:" jobTitle={job.workingSchedule} />
         </div>
       </div>
     </div>
